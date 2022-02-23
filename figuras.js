afd417738e589de5f3ce4f6f2d1546b77bb99431
GitEdy2 <clanuzumaki22@gmail.com>
2022-02-22 21:07:29 -0500

    cambios figuras

--- figuras.js
+++ figuras.js
@@ -1,69 +1,85 @@
-console.group("Cuadrados");
-
+// Codigo del cuadrado
 function perimetroCuadrado(lado) {
     return lado * 4;
-}
-
+};
 function areaCuadrado(lado) {
     return lado * lado;
-}
+};
 
-console.groupEnd();
-
-//---------------------------------------
-
-console.group("Triangulos");
-
+// Codigo del triangulo
 function perimetroTriangulo(lado1, lado2, base) {
     return lado1 + lado2 + base;
 }
-
-function areaTriangulo(base,altura) {
-    return (base*altura) / 2;
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
 }
 
-console.groupEnd();
-
-//-----------------------------------------
-
-console.group("Circulos");
-
+// Codigo del Circulo
 function diametroCirculo(radio) {
     return radio * 2;
 }
 
-const PI = Math.PI;
-console.log("PI es: " + PI);
+// PI
+const PI = Math.PI.toFixed(2);
 
+// Perimetro
 function perimetroCirculo(radio) {
     const diametro = diametroCirculo(radio);
-
     return diametro * PI;
-}
+} 
 
+// Area
 function areaCirculo(radio) {
-    return (radio*radio) * PI;
+    return (radio * radio) * PI;
 }
 
-console.groupEnd();
-
-//------------------------------------------
-
-function calcularPerimetroCuadrado() {
-    const input = document.getElementById("InputCuadrado");
-    const value = input.value;
-
-    const perimetro = perimetroCuadrado(value);
-    alert(perimetro);
+// Calcular Triangulo
+function calcularTriangulo() {
+    const inputAltura = document.getElementById("inputAltura");
+    const inputBase = document.getElementById("inputBase");
+    const inputA = document.getElementById("inputA");
+    const inputB = document.getElementById("inputB");
+    const resArea = document.getElementById("aResult");
+    const resPerimetro = document.getElementById("pResult");
+    const h = parseInt(inputAltura.value);
+    const bs = parseInt(inputBase.value);
+    const a = parseInt(inputA.value);
+    const b = parseInt(inputB.value);
+    const perimetro = perimetroTriangulo(a, b, bs);
+    const area = areaTriangulo(bs, h);
+    resPerimetro.innerHTML = perimetro + " cm";
+    resArea.innerHTML = area + " cm";
 }
 
-function calcularAreaCuadrado() {
-    const input = document.getElementById("InputCuadrado");
-    const value = input.value;
-
-    const area = areaCuadrado(value);
-    alert(area);
+// Calcular Circulo
+function calcularCirculo() {
+    const inputRadio = document.getElementById("inputRadio");
+    const resArea = document.getElementById("aResult");
+    const resPerimetro = document.getElementById("pResult");
+    const r = parseInt(inputRadio.value);
+    const area = areaCirculo(r)
+    const perimetro = perimetroCirculo(r);
+    resPerimetro.innerHTML = perimetro.toFixed(2) + " cm";
+    resArea.innerHTML = area.toFixed(2) + " cm";
 }
 
-//------------------------------------------
+// Calcular Cuadrado
+function calcularCuadrado() {
+    const inputLados = document.getElementById("inputLados");
+    const resArea = document.getElementById("aResult");
+    const resPerimetro = document.getElementById("pResult");
+    const lado = parseInt(inputLados.value);
+    const area = areaCuadrado(lado)
+    const perimetro = perimetroCuadrado(lado);
+    resPerimetro.innerHTML = perimetro.toFixed(2) + " cm";
+    resArea.innerHTML = area.toFixed(2) + " cm";
+}
 
+// Restablecer formulario
+function restablecer() {
+    const resArea = document.getElementById("aResult");
+    const resPerimetro = document.getElementById("pResult");
+    document.getElementById("form").reset();
+    resPerimetro.innerHTML = 0 + " cm";
+    resArea.innerHTML = 0 + " cm";
+}
